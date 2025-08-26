3ae39c3c34fd3d232cc21a09bb1b0c5fdde0d037
Own3r <darkwebsite95@gmail.com>
2025-08-26 11:01:23 +0330

    fixed the bug which added unwanted margin to the bottom of the app after going to background

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,14 @@
 import LayoutScreen from "@/components/LayoutScreen";
 import React from "react";
 import { StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 
 export default function Layout() {
   return (
-    <>
+    <SafeAreaView>
       <LayoutScreen />
       <StatusBar barStyle={"light-content"} />
-    </>
+    </SafeAreaView>
   );
 }
