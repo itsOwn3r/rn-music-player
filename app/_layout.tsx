62294e251e8f4cd81a90f93214d77978ce81772a
Own3r <darkwebsite95@gmail.com>
2025-09-03 13:21:01 +0330

    Deleted all react-native-track-player related codes. Switching back to expo-audio.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,10 @@
-import { useSetupTrackPlayer } from "@/hooks/useSetupTrackPlayer";
-import { SplashScreen, Stack } from "expo-router";
-import React, { useCallback } from "react";
+import { Stack } from "expo-router";
+import React from "react";
 import { StatusBar } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
 
-// SplashScreen.preventAutoHideAsync();
-
 export default function Layout() {
-  console.log("Hellpo awsd aws");
-  const handleTrackPlayerLoaded = useCallback(() => {
-    SplashScreen.hideAsync();
-  }, []);
-  useSetupTrackPlayer({ onLoad: handleTrackPlayerLoaded });
-  console.log("Hellpo");
   return (
     <SafeAreaProvider>
       {/* <SafeAreaView style={{ flex: 1 }}> */}
