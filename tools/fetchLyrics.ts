a5cdd4ed45cb6c17fecabe704d1a3f32e6d47a4e
Own3r <darkwebsite95@gmail.com>
2026-09-21 12:13:19 +0330

    Fixed fetching the lyrics

--- tools/fetchLyrics.ts
+++ tools/fetchLyrics.ts
@@ -6,6 +6,7 @@ export const fetchLyrics = async (
   track: Song | null,
   setLyrics: (id: string, plainLyrics: string, syncedLyrics?: string) => void
 ) => {
+  console.log("OKKKKK");
   if (!track) {
     return null;
   }
@@ -15,6 +16,12 @@ export const fetchLyrics = async (
       `https://lrclib.net/api/get?artist_name=${encodeURI(track.artist || "").replace(/%20/g, "+")}&track_name=${encodeURI(track.title || "").replace(/%20/g, "+")}`,
       {
         method: "GET",
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+          Origin: "https://lrclib.net",
+          Referer: "https://lrclib.net/",
+        },
       }
     );
 
